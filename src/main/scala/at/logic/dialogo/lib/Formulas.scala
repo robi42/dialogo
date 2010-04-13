9fd0990a2ebbcadd5b8fcfb044297bd41ea61dda
Robert Thurnher <r.thurnher@gmail.com>
2010-04-14 00:21:49 +0200

    Refine HTML entity handling.

--- src/main/scala/at/logic/dialogo/lib/Formulas.scala
+++ src/main/scala/at/logic/dialogo/lib/Formulas.scala
@@ -1,5 +1,7 @@
 package at.logic.dialogo.lib
 
+import xml.Unparsed
+
 object Formulas {
   var latestId = 0L
 
@@ -14,7 +16,7 @@ trait AtomicExpression {
   val isAtomic = true
   val atom: Atom
 
-  def toHtml = <span id={id}>{atom.name}</span>
+  def toHtml = <span id={id}>{Unparsed(atom.name)}</span>
 }
 
 abstract case class Expression(leftHandSide: AtomicExpression,
@@ -23,57 +25,58 @@ abstract case class Expression(leftHandSide: AtomicExpression,
   override val isAtomic = false
 
   override def toHtml =
-    <span id={id}>({leftHandSide.toHtml}{atom.name}{rightHandSide.toHtml})</span>
+    <span id={id}>({leftHandSide.toHtml}{Unparsed(atom.name)}{rightHandSide.toHtml})</span>
 }
 
 case class Atom(name: String)
 
-case class Operator(value: Any, override val name: String) extends Atom(name)
+case class Operator(override val name: String, value: Any) extends Atom(name)
 
 case class Operand(name: String) extends AtomicExpression {
   override val atom = Atom(name)
 }
 
 case class Falsum extends AtomicExpression {
-  override val atom = Operator(false, "\u22A5")
+  override val atom = Operator("&#x22A5;", false)
 }
 
 case class Verum extends AtomicExpression {
-  override val atom = Operator(true, "T")
+  override val atom = Operator("T", true)
 }
 
 case class Negation(rightHandSide: AtomicExpression) extends AtomicExpression {
-  override val atom = Operator("!", "\u00AC")
+  override val atom = Operator("&#x00AC;", "!")
 
-  override def toHtml = <span id={id}>{atom.name}({rightHandSide.toHtml})</span>
+  override def toHtml =
+    <span id={id}>{Unparsed(atom.name)}({rightHandSide.toHtml})</span>
 }
 
 case class Implication(override val leftHandSide: AtomicExpression,
                        override val rightHandSide: AtomicExpression)
         extends Expression(leftHandSide, rightHandSide) {
-  override val atom = Operator("->", "\u2192")
+  override val atom = Operator("&#x2192;", "->")
 }
 
 case class And(override val leftHandSide: AtomicExpression,
                override val rightHandSide: AtomicExpression)
         extends Expression(leftHandSide, rightHandSide) {
-  override val atom = Operator("&", "\u2227")
+  override val atom = Operator("&#x2227;", "&")
 }
 
 case class SuperAnd(override val leftHandSide: AtomicExpression,
                     override val rightHandSide: AtomicExpression)
         extends Expression(leftHandSide, rightHandSide) {
-  override val atom = Operator("&&", "\u2299")
+  override val atom = Operator("&#x2299;", "&&")
 }
 
 case class Or(override val leftHandSide: AtomicExpression,
               override val rightHandSide: AtomicExpression)
         extends Expression(leftHandSide, rightHandSide) {
-  override val atom = Operator("|", "\u2228")
+  override val atom = Operator("&#x2228;", "|")
 }
 
 case class SuperOr(override val leftHandSide: AtomicExpression,
                    override val rightHandSide: AtomicExpression)
         extends Expression(leftHandSide, rightHandSide) {
-  override val atom = Operator("||", "\u2295")
+  override val atom = Operator("&#x2295;", "||")
 }
--- src/test/scala/at/logic/dialogo/lib/DomainModelTestSpecs.scala
+++ src/test/scala/at/logic/dialogo/lib/DomainModelTestSpecs.scala
@@ -2,6 +2,7 @@ package at.logic.dialogo.lib
 
 import org.specs.Specification
 import org.specs.runner.{ConsoleRunner, JUnit3}
+import xml.Unparsed
 
 class DomainModelTestSpecsAsTest extends JUnit3(DomainModelTestSpecs)
 object DomainModelTestSpecsRunner extends ConsoleRunner(DomainModelTestSpecs)
@@ -9,7 +10,7 @@ object DomainModelTestSpecsRunner extends ConsoleRunner(DomainModelTestSpecs)
 object DomainModelTestSpecs extends Specification {
   val formula = Implication(And(Operand("a"), Operand("b")), Operand("c"))
   val expectedHtml =
-    <span id="expression5">(<span id="expression3">(<span id="expression1">a</span>\u2227<span id="expression2">b</span>)</span>\u2192<span id="expression4">c</span>)</span>
+    <span id="expression5">(<span id="expression3">(<span id="expression1">a</span>{Unparsed("&#x2227;")}<span id="expression2">b</span>)</span>{Unparsed("&#x2192;")}<span id="expression4">c</span>)</span>
 
   "A domain model formula" should {
     "be representable as HTML" in {
